bca87c4d39863f2f6170766cde82c1ca9af78b0a
Ben Vaccaro <freaksed@gmail.com>
2019-11-16 18:03:17 -0800

    stub in bandsintown query

---
+++ Assets/src/BandsInTownAPI.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+public class BandsInTownAPI : MonoBehaviour
+{
+    private const string APIKey = "nTG4tbSXpIaniCHlJ62q06GzIpROk6qh56EiK7N1";
+    private const string  URI = "https://search.bandsintown.com/search";
+
+    private void SendRequest()
+    {
+        float latitude = 32, longitude = -117;
+
+        var query = new JObject();
+        query.Add("location", "region");
+        query.Add("region", new JObject
+        {
+            new JObject("latitude", latitude),
+            new JObject("longitude", longitude),
+            new JObject("radius", 50)
+        });
+    }
+}
